5d1081a61fd3f39eb5ff5f7ae2cf745cd8dbcb21
Dung-048 <minhdung04082003@gmail.com>
2025-04-02 20:22:15 +0700

    add url-safety-check

--- backend/src/modules/urls/url.service.ts
+++ backend/src/modules/urls/url.service.ts
@@ -1,4 +1,5 @@
 import {BadRequestException, Injectable, NotFoundException} from '@nestjs/common';
+import axios from 'axios';
 import {InjectRepository} from '@nestjs/typeorm';
 import {Repository} from 'typeorm';
 import {UrlEntity} from './entities/url.entity';
@@ -9,42 +10,54 @@ import {generateCode} from "../../utils/code-utils";
 
 @Injectable()
 export class UrlService {
+    private apiKey = 'da1b859bde1ec66b908f100e674b283173750abb829ac49e3839a887486b63bc';
+
     constructor(
         @InjectRepository(UrlEntity)
         private readonly urlRepository: Repository<UrlEntity>,
     ) {}
 
     async createUrl(urlRequestDto: UrlRequest, user: UserEntity): Promise<Url> {
+
         const { originalUrl, shortCode } = urlRequestDto;
-
         const verifiedShortCode = await this.verifyOrGenerateShortCode(shortCode);
-
+        const safetyScore = await this.checkUrlSafety(originalUrl);
         const newUrl = await this.urlRepository.save({
-            originalUrl,
-            shortCode: verifiedShortCode,
-            user,
-        });
+                originalUrl,
+                shortCode: verifiedShortCode,
+                user,
+                safetyScore,
+            });
 
-        return Url.fromEntity(newUrl);
+            return Url.fromEntity(newUrl);
     }
+
+    async checkUrlSafety(url: string): Promise<number> {
+        const encodedUrl = Buffer.from(url).toString('base64');
+        const response = await axios.get(`https://www.virustotal.com/api/v3/urls/${encodedUrl}`, {
+            headers: {
+                'x-apikey': this.apiKey,
+            },
+        });
+        const safetyScore = response.data.data.attributes.last_analysis_stats.malicious || 0;
+        return safetyScore;
+    }
+
 
     private async verifyOrGenerateShortCode(shortCode?: string): Promise<string> {
         if (!shortCode) {
             return await this.generateUniqueCode();
         }
-
         const existingUrl = await this.urlRepository.findOneBy({ shortCode });
         if (existingUrl) {
             throw new BadRequestException('Mã đã tồn tại');
         }
-
         return shortCode;
     }
 
     private async generateUniqueCode(): Promise<string> {
         const code = generateCode();
-
-        return (await this.urlRepository.existsBy({shortCode: code})) ? await this.generateUniqueCode() : code;
+        return (await this.urlRepository.existsBy({ shortCode: code })) ? await this.generateUniqueCode() : code;
     }
 
     private async findUrlOrThrow(criteria: Partial<UrlEntity>): Promise<UrlEntity> {
