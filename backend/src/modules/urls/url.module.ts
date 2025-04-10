abdb7ea70330aa42c2bfa0c14debbf1ecb231d3d
Lê Minh Dũng <minhdung04082003@gmail.com>
2025-04-10 09:26:41 +0700

    add url-safety-check (#4)
    * add url-safety-check
    
    * update url-safety-check
    
    * update lan 2 url-safety-check
    
    * update lan 3url-safety-check

--- backend/src/modules/urls/url.module.ts
+++ backend/src/modules/urls/url.module.ts
@@ -1,13 +1,14 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { UrlController } from './url.controller';
-import { UrlService } from './url.service';
-import { UrlEntity } from './entities/url.entity';
+import {Module} from '@nestjs/common';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {UrlController} from './url.controller';
+import {UrlService} from './url.service';
+import {UrlEntity} from './entities/url.entity';
+import {VirusTotalService} from "./service/virustotal.service";
 
 @Module({
     imports: [TypeOrmModule.forFeature([UrlEntity])],
     controllers: [UrlController],
-    providers: [UrlService],
-    exports: [UrlService],
+    providers: [UrlService,VirusTotalService],
+    exports: [UrlService, VirusTotalService],
 })
 export class UrlModule {}
